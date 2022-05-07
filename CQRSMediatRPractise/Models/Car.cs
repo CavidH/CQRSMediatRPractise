8adc81c1f710dc6015f9e2770a64952062418a56
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-05-07 19:51:00 +0400

    Add project files.

---
+++ CQRSMediatRPractise/Models/Car.cs
@@ -0,0 +1,10 @@
+﻿// Cavid Haciyev
+// 2022/05/07   19:50
+
+namespace CQRSMediatRPractise.Models
+{
+    public class Car
+    {
+        
+    }
+}
---
+++ CQRSMediatRPractise/Models/CarModel.cs
@@ -0,0 +1,10 @@
+﻿// Cavid Haciyev
+// 2022/05/07   19:49
+
+namespace CQRSMediatRPractise.Models
+{
+    public class CarModel
+    {
+        
+    }
+}
